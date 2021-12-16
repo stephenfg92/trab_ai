13ca84951afafbe6833b4baf5239e9cd209ee2ce
Stephen Freitas <stephenfg@outlook.com>
2021-12-16 15:04:33 -0300

    Treinamento bem sucedido. Criado notebook para demonstrar resultados

---
+++ treinamento.py
@@ -0,0 +1,76 @@
+import keras as keras
+import numpy as np
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from sklearn.utils import class_weight
+
+from sklearn.model_selection import train_test_split
+
+MODEL_EXPORT_PATH = 'trained_model/'
+
+# passo 0
+def le_arquivo(nome_arq):
+    arq = open(nome_arq, 'r')  # abre o arquivo
+    csvreader = csv.reader(arq, delimiter=",")
+    headers = next(csvreader, None)
+    rows = []
+    for row in csvreader:
+        rows.append(row)
+
+    arq.close()
+
+    rows = np.array(rows)
+
+    rows = rows.astype(float)
+
+    return rows
+
+
+ct = le_arquivo('resumo.csv')
+
+num_padroes, num_atrib = ct.shape
+
+X = ct[:, 0:num_atrib - 1]
+Y = ct[:, num_atrib - 1:num_atrib]
+
+# Stratify é amostragem estratificada. É um conceito de estatística usado quando os dados estão desbalanceados.
+# Aqui, por exemplo, temos muitos dados onde attack == 1, versus 477 de attack == 0.
+XTraining, XValidation, YTraining, YValidation = train_test_split(X,Y,stratify=Y,test_size=0.1) # before model building
+
+n0 = 200
+n1 = 200
+
+model = keras.models.Sequential()
+model.add(keras.layers.Input(shape=[num_atrib - 1]))
+model.add(keras.layers.Dense(n0, activation="relu"))
+model.add(keras.layers.Dense(n1, activation="relu"))
+model.add(keras.layers.Dense(1, activation="sigmoid"))
+
+ts = 20
+lr = 0.1
+
+optimizer = keras.optimizers.Adam(learning_rate=lr, decay=lr / ts)
+
+model.compile(loss="binary_crossentropy", optimizer=optimizer, metrics=["accuracy"])
+
+# class_weights = class_weight.compute_class_weight('balanced', np.unique(y), y)
+
+history = model.fit(XTraining, YTraining, epochs=ts, validation_data=(XValidation,YValidation), shuffle=True)
+
+model.save(MODEL_EXPORT_PATH)
+
+pd.DataFrame(history.history).plot(figsize=(8, 5))
+plt.grid(True)
+plt.gca().set_ylim(0, 2)
+plt.show()
+
+x_new = X[1:2, :]
+
+print(x_new)
+
+y_new = model.predict(x_new)
+
+print(y_new)
